4096597ac652e345bb9504ccdbc34b143266eaca
kuuuuumiiiii <129586216+kuuuuumiiiii@users.noreply.github.com>
2023-04-29 13:19:47 +0000

    Add 注文情報確認画面編集

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,14 +6,32 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-
-    @shipping_address = Address.find(params[:order][:address_id])
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.shipping_address
-    @order.name = current_customer.last_name + current_customer.first_name
     @postage = 800
     @total = 0
     @cart_items = current_customer.cart_items
+
+    if params[:order][:select_shipping_address] == "0"
+      #登録しているcustomerモデルの住所を取得する
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+
+    elsif params[:order][:select_shipping_address] == "1"
+      #お届け先に登録している住所shipping_addressモデルから取得する
+      @shipping_address = ShippingAddress.find(params[:order][:shipping_address_id])
+      @order.postal_code = @shipping_address.postal_code
+      @order.address = @shipping_address.address
+      @order.name = @shipping_address.last_name + @shippinng_address.first_name
+
+    else params[:order][:select_shipping_address] == "2"
+      #新しいお届け先の場合
+
+
+    end
+
+
+
+
   end
 
   def completed
