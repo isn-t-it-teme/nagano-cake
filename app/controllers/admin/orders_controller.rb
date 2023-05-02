8662a4e30e6d6684bc2ae16943344b9b86c0602a
kuuuuumiiiii <129586216+kuuuuumiiiii@users.noreply.github.com>
2023-05-02 10:07:28 +0000

    Add 管理者側の注文履歴詳細画面編集

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,6 +6,22 @@ class Admin::OrdersController < ApplicationController
     @customer = @order.customer
   end
 
+  #注文ステータスの更新
+  def update
+    #注文詳細データの取得
+    @order = Order.find(params[:id])
+    #注文ステータスの更新
+    @order.update(order_params)
+    #注文商品の注文ステータスが「入金確認」だったら
+    
+    #注文商品の制作ステータスを「制作待ち」に更新
+    
+    redirect_to admin_order_path
+  end
+
+
+  private
+
   def order_params
     params.require(:order).permit(:payment, :postal_code, :address, :name, :customer_id, :total_price, :postage, :status)
   end
