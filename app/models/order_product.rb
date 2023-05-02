5593548062bce00c16526207a28872a82b0a47e0
kuuuuumiiiii <129586216+kuuuuumiiiii@users.noreply.github.com>
2023-05-02 03:19:06 +0000

    Add 管理者側の注文履歴一覧編集

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,7 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :item
+  
+  #制作ステータスのenum
+  enum status: {cannot_start: 0, waiting_for_payment: 1, in_production: 2, completion_of_production: 3}
 end
